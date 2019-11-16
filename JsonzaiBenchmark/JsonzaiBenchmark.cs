6551e5c0417dc15f3bbc33f66d30a1146dcbaa9a
carolinacouto98 <carolinacouto98@users.noreply.github.com>
2019-11-16 01:08:47 +0000

    b

---
+++ JsonzaiBenchmark/JsonzaiBenchmark.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonzaiBenchmark
+{
+    public class JsonzaiBenchmarkConfig : ManualConfig
+    {
+        public JsonzaiBenchmarkConfig()
+        {
+            Add(Job.MediumRun
+                   .WithLaunchCount(1)
+                   .With(InProcessEmitToolchain.Instance)
+                   .WithId("InProcess"));
+        }
+    }
+
+    [RankColumn]
+    [Config(typeof(JsonzaiBenchmarkConfig))]
+
+    class JsonzaiBenchmark
+    {
+        string src = "{Name: \"Ze Manel\", Nr: 6512, Group: 11, github_id: \"omaior\"}";
+        JsonParsemit
+        JsonParser
+        Student std = (Student)JsonParsemit.Parse(src, typeof(Student));
+        Assert.AreEqual("Ze Manel", std.Name);
+            Assert.AreEqual(6512, std.Nr);
+            Assert.AreEqual(11, std.Group);
+            Assert.AreEqual("omaior", std.GithubId);
+    }
+}
